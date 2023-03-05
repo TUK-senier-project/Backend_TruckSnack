0c70943a5e59e3668961ed26da9f316dea0414c3
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-03-06 00:24:12 +0900

    Customer 예외처리 기능 구현 완료 .. 인터페이스 작업중

--- src/main/java/Backend_TruckSnack/TruckSnack/function/CustomerFunction.java
+++ src/main/java/Backend_TruckSnack/TruckSnack/function/CustomerFunction.java
@@ -3,34 +3,61 @@ package Backend_TruckSnack.TruckSnack.function;
 public class CustomerFunction {
     /**
      * 컨트롤러에서 처리할 것들
-     * 아이디 : 공백 확인 , 특수문자 확인 , 길이 확인(5~15)
-     * 패스워드 : 공백확인,길이확인(20) , 문자가 영+숫 , (한예외처리)
-     * 이름 : 공백확인 , 길이 확인(10글자 이내) ,
+     * 아이디 : 공백 확인 , 특수문자 확인 , 길이 확인(5~15) ,(한예외 처리)
+     * 패스워드 : 공백확인,길이확인(8~20) , 문자가 영+숫 , (한예외처리)
+     * 이름 : 공백확인 , 길이 확인(10글자 이내) , (한글이름만 허용)
      * 핸드폰 번호 : 11 글자인지 , 숫자로만 이루어져 있는지
      * 지역 : 50글자 이내인지 확인
      */
-
+    // 공백확인 함수 Start
+    public int null_check(String customer_data){
+        if(customer_data == null || "".equals(customer_data)){
+            return 1;
+        }
+        return 0;
+    }
+    public int korean_check(String customer_data){
+        if(customer_data.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    // 공배확인 함수 End
     //id check Start
     public int customer_id_check(String customer_id){
         int check_flag = 0; //0 성공 , 1 실패
 
         //공백 경우
-        if(customer_id == null || "".equals(customer_id)){
-            check_flag = 1;
-        }
-        //5~15글자가 아닌경우
-        else if(customer_id.length()>15 || customer_id.length()<5){
-            check_flag = 1;
-        }
-        //특수문자  확인
-        else{
-            customer_id  = customer_id .replaceAll("\\p{Z}", "");
-            if (!customer_id.matches("[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝|(|)|.|-]*")) {
-                check_flag = 1;
+        check_flag = null_check(customer_id);
+
+        if(check_flag == 0){
+            check_flag = korean_check(customer_id);
+            if(check_flag == 0){
+                //5~15글자가 아닌경우
+                if(customer_id.length()>15 || customer_id.length()<5){
+                    check_flag = 1;
+                }
+                //특수문자  확인
+                else{
+                    customer_id  = customer_id .replaceAll("\\p{Z}", "");
+                    if (!customer_id.matches("[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝|(|)|.|-]*")) {
+                        check_flag = 1;
+                    }
+                    else{
+                        check_flag = 0;
+                    }
+                }
             }
             else{
-                check_flag = 0;
+                // 아이디에 한글이 포함된경우
+                return 1;
             }
+        }
+        else{
+            //공백이 있는경우
+            check_flag = 1;
         }
 
 
@@ -41,27 +68,121 @@ public class CustomerFunction {
     //password check Start
     public int customer_password_check(String customer_password){
         int check_flag = 0; //0 성공 , 1 실패
+        //공백 경우
+        check_flag = null_check(customer_password);
+
+        if(check_flag == 0){
+            check_flag = korean_check(customer_password);
+            if(check_flag == 0){
+                //8~20글자가 아닌경우
+                if(customer_password.length()>20 ||customer_password.length()<8){
+                    check_flag = 1;
+                }
+                else {
+                    check_flag =0 ;
+                }
+            }
+            else{
+                // 아이디에 한글이 포함된경우
+                return 1;
+            }
+        }
+        else{
+            //공백이 있는경우
+            check_flag = 1;
+        }
+
+
         return check_flag;
     }
     //password check End
 
     //name check Start
-    public int customer_name_check(){
+    public int customer_name_check(String customer_name){
         int check_flag = 0; //0 성공 , 1 실패
+        //공백 경우
+        check_flag = null_check(customer_name);
+
+        if(check_flag == 0){
+            //한글로만 이루어진 경우 허용
+            if(!customer_name.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")){
+                return 1; //한글이외에 다른 문자가 들어옴
+            }
+            //이름이 10글자 이내
+            else if (customer_name.length()>10){
+                return 1; //10글자 이상의 이름
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            //공백이 있는경우
+            check_flag = 1;
+        }
+
+
         return check_flag;
     }
     //name check End
 
     //phone_number check Start
-    public int customer_phone_number_check(){
+    public int customer_phone_number_check(String customer_phone_number){
         int check_flag = 0; //0 성공 , 1 실패
+        //공백 경우
+        check_flag = null_check(customer_phone_number);
+
+        if(check_flag == 0){
+            //숫자만 허용
+            if(!customer_phone_number.matches(".*[0-9]+.*")){
+                return 1; //숫자이외에 다른 값이 들어옴
+            }
+            //11 자리가 아닌 경우
+            else if (customer_phone_number.length() !=10){
+                return 1; //11글자가 이닌 숫자가 들어옴
+            }
+            else{
+                //정상처리
+                return 0;
+            }
+        }
+        else{
+            //공백이 있는경우
+            check_flag = 1;
+        }
+
+
         return check_flag;
     }
     //phone_number check End
 
     //location check Start
-    public int customer_location_check(){
+    public int customer_location_check(String customer_location){
         int check_flag = 0; //0 성공 , 1 실패
+        //공백 경우
+        check_flag = null_check(customer_location);
+
+        if(check_flag == 0){
+            //특수문자 비허용
+            customer_location  = customer_location.replaceAll("\\p{Z}", "");
+            if (!customer_location.matches("[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝|(|)|.|-]*")) {
+                //특수문자가 들어았는 경우
+                check_flag = 1;
+            }
+            //정보가 50글자가 넘는경우
+            else if (customer_location.length()>50){
+                return 1; //50글자 이상의 내용
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            //공백이 있는경우
+            check_flag = 1;
+        }
+
+
         return check_flag;
     }
     //location check End
