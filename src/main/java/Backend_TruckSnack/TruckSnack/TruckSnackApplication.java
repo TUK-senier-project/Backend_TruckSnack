2fc94c132db19bd0e81834b659d9c23eb41972c4
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-03-07 20:15:14 +0900

    s3 img upload 기능 실패.. 다른방법으로

--- src/main/java/Backend_TruckSnack/TruckSnack/TruckSnackApplication.java
+++ src/main/java/Backend_TruckSnack/TruckSnack/TruckSnackApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TruckSnackApplication {
-
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TruckSnackApplication.class, args);
 	}
