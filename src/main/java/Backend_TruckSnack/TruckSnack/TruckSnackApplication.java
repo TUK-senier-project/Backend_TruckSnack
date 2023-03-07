ac5763ae298f7f98a814ac8c41d915d65eaa702a
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-03-07 20:16:20 +0900

    s3 img upload 기능 실패.. 다른방법으로

--- src/main/java/Backend_TruckSnack/TruckSnack/TruckSnackApplication.java
+++ src/main/java/Backend_TruckSnack/TruckSnack/TruckSnackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TruckSnackApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TruckSnackApplication.class, args);
 	}
