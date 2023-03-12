f8e1442b279358e478ad66d98e1c0a22688f66e7
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-03-13 05:59:11 +0900

    foodList finder 조회 정보 완료

--- src/main/java/Backend_TruckSnack/TruckSnack/repository/mapping/FoodListMapping.java
+++ src/main/java/Backend_TruckSnack/TruckSnack/repository/mapping/FoodListMapping.java
@@ -1,5 +1,8 @@
 package Backend_TruckSnack.TruckSnack.repository.mapping;
 
 public interface FoodListMapping {
-    String getPassword();
+    String getBusinessName();
+    int getDeadline();
+    String getPhoneNumber();
+    String getSellerImgS3Url();
 }
