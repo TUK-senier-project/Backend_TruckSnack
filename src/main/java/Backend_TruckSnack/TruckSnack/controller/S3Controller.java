65f2d094287d9831702b5aaf9a6fd397e826f7f6
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-04-06 01:32:26 +0900

    AWS S3 Img 받아서 리턴하기 Test 작성 완료

--- src/main/java/Backend_TruckSnack/TruckSnack/controller/S3Controller.java
+++ src/main/java/Backend_TruckSnack/TruckSnack/controller/S3Controller.java
@@ -1,13 +1,19 @@
 package Backend_TruckSnack.TruckSnack.controller;
 
+import Backend_TruckSnack.TruckSnack.domain.Seller;
+import Backend_TruckSnack.TruckSnack.repository.dto.RecommendOrderDataDTO;
+import Backend_TruckSnack.TruckSnack.service.S3Service;
 import Backend_TruckSnack.TruckSnack.service.S3Upload;
 import Backend_TruckSnack.TruckSnack.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +24,8 @@ import java.io.IOException;
 @RestController
 public class S3Controller {
     private final S3Upload s3Upload;
+
+    private final S3Service s3Service;
 
     @PostMapping("/upload-Test")
     public ResponseEntity<ApiResponse<String>> uploadFileTest(@RequestParam("images") MultipartFile multipartFile) throws IOException {
@@ -38,6 +46,16 @@ public class S3Controller {
             return ResponseEntity.status(response.getStatus()).body(response);
         }
     }
+    @PostMapping("/getImg-Test")
+    public ResponseEntity getImgTest(@RequestBody Seller sellerData) throws IOException {
+         String  seller_id = sellerData.getId();
+        InputStreamResource resource = s3Service.s3_img_seller_main_return_service(seller_id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(resource);
+    }
+
+
 
 
     public ResponseEntity<ApiResponse<String>> uploadFile(@RequestParam("images") MultipartFile multipartFile) throws IOException {
