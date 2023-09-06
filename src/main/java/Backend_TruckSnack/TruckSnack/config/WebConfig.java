50560db7cc8f58e49e8bb9522d6771ea68b31cc1
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-09-07 02:00:00 +0900

    front

---
+++ src/main/java/Backend_TruckSnack/TruckSnack/config/WebConfig.java
@@ -0,0 +1,15 @@
+package Backend_TruckSnack.TruckSnack.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
---
+++ src/main/java/Backend_TruckSnack/TruckSnack/controller/webController.java
@@ -0,0 +1,14 @@
+package Backend_TruckSnack.TruckSnack.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class webController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
